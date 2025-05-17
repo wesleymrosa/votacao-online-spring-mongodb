47d36b12f41ae81fcdb23b1abc6737301764dfd3
wesleymrosa <wesleymrosa@gmail.com>
2025-05-17 08:05:21 -0300

    Inserido projeto base gerado pelo Spring Initializr

---
+++ src/main/java/br/wesley/mongo_spring_lab/votacao/VotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.mongo_spring_lab.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/wesley/mongo_spring_lab/votacao/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.mongo_spring_lab.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
